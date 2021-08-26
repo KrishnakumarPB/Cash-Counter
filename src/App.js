f4275d53a78f191c76dbdbaf52d32dde35886238
Krishnakumar <krishnakumar.amritham@gmail.com>
2021-08-26 07:45:18 +0530

    UI Changes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ let reminder="";
 let currentamount;
 let i="";
 var value="";
+var label="";
+let amountreceived=""
+let differenceamount="";
 
 currencyarr.sort( function( b,a){
   if(a > b) return 1;
@@ -20,69 +23,134 @@ class App extends Component {
   constructor(){
     super()
       this.state={
-        amount:""
+        billamount:"",
+        receivedamount:"",
+        currencydenominations:""
       }
+      this.receivedInput = React.createRef();
       this.handleChange=this.handleChange.bind(this);
-    
+      this.handleSubmit=this.handleSubmit.bind(this)
   }
 handleChange(event){
+    value=event.target.value;
+    label=event.target.name;
+    this.setState({
+      [label]:value ,
+      currencydenominations:""
+    })
+    if(label=="receivedamount")
+    {
+      this.receivedInput.current.focus();
+    }
     denominations={};
     quotient="";
     reminder="";
     currentamount="";
     i="";
-    value="";
-    console.log(event.target);
-    value=event.target.value;
-    this.setState({
-      amount:value,
-      currencydenominations:""
-    })
-    this.handleSubmit=this.handleSubmit.bind(this)
-    this.handleChange=this.handleChange.bind(this)
+    
   }
-//************************Logic****************************************
+//****************************************Logic Start*********************************************************
 handleSubmit(){
-currentamount=value;
-currentamount=Math.floor(currentamount)
-for (i=0;i<currencyarr.length;i++)
-{
-if (currencyarr[i]<= currentamount && (reminder ==="" || reminder !==0))
-{
-  console.log(`before reminder${reminder}`)
-  quotient=Math.floor(currentamount/currencyarr[i]);
-  denominations[currencyarr[i]]=quotient;
-  reminder=Math.floor(currentamount%currencyarr[i]);
-  console.log(`reminder${reminder}`)
-  if (reminder !== 0)
+  denominations={};
+  quotient="";
+    reminder="";
+    currentamount="";
+    i="";
+  amountreceived=document.getElementById("receivedamount").value;
+  amountreceived=Math.floor(amountreceived)
+  if(amountreceived != "")
   {
-    currentamount=reminder;
-    // console.log(`reminder${reminder}`)
+    console.log(value);
+    value=Math.floor(value)
+    currentamount=amountreceived-value;
+    differenceamount=currentamount;
+    console.log(currentamount);
+    if (currentamount>0)
+    {
+      currentamount=Math.floor(currentamount)
+      
+      for (i=0;i<currencyarr.length;i++)
+      {
+      if (currencyarr[i]<= currentamount && (reminder ==="" || reminder !==0))
+      {
+        console.log(`before reminder${reminder}`)
+        quotient=Math.floor(currentamount/currencyarr[i]);
+        denominations[currencyarr[i]]=quotient;
+        reminder=Math.floor(currentamount%currencyarr[i]);
+        console.log(`reminder${reminder}`)
+        if (reminder !== 0)
+        {
+          currentamount=reminder;
+          // console.log(`reminder${reminder}`)
+        }
+        else{
+          break;
+        }
+        console.log(`i=${i} and quotient =${quotient} and reminder=${reminder} /currency=${currencyarr[i]}`)
+      }
+  
+      }
+      this.setState({
+        currencydenominations:Object.entries(denominations).map(([key,value])=>{
+              return (  <Results currency={key} number= {value.toString()}/>)
+          
+        })
+      })
+      console.log(denominations);
+    }
+  }
+  const element=document.getElementById("receivedamount")
+  element.value=28;
+  // console.log(element.value={amountreceived})
+}
+//*****************************************Logic Finished************************************************* */
+render(){
+function RcvdAmountContainer(props){ 
+  const amountbilled=props.amountbilled;
+  if(amountbilled=="")
+  {
+    return(
+      <div></div>
+    )
   }
   else{
-    break;
+    return(
+        <div>
+
+              <label for="receivedamount">Enter the Received Amount:</label>
+              <input className="input" id="receivedamount" type="number" name="receivedamount" 
+               ref={props.receivedInput} 
+               ></input>
+              <button className="btn btn-primary" onClick={props.handleSubmit}>Submit</button>
+              
+        </div>
+    )
   }
-  console.log(`i=${i} and quotient =${quotient} and reminder=${reminder} /currency=${currencyarr[i]}`)
 }
-
-}
-this.setState({
-  currencydenominations:Object.entries(denominations).map(([key,value])=>{
-         return (  <Results currency={key} number= {value.toString()}/>)
-    
-  })
-})
-}
-  render(){
-  
-
   return (
     <div className="App">
       <div className="container-center">
         <div className="interface">
           <h1>The Cash Counter</h1>
-          <input className="input"  type="number" onChange={this.handleChange} name="amount" value={this.state.amount}></input>
-          <button className="btn btn-primary" onClick={this.handleSubmit}>Submit</button>
+          <table className="input-table">
+            <tr>
+              <td >
+                <label for="billamount">Enter the Total Bill Amount:</label>
+                <input className="input" id="billamount" type="number" onChange={this.handleChange} name="billamount" value={this.state.billamount}></input>               
+              </td>
+              <td >
+                <RcvdAmountContainer 
+                amountbilled={this.state.billamount} 
+                handleSubmit={this.handleSubmit}
+                receivedamount={this.state.receivedamount}
+                />
+              </td>
+            </tr>
+              
+                Remaining Amount={differenceamount}
+    
+          </table>      
+          <h1>{this.state.receivedamount}</h1>
           <div className="result-container">
           {this.state.currencydenominations}
           </div>
